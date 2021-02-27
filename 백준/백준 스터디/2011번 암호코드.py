27cb940c35336b0125f643c4c49e75c6f97c6176
choi1087 <adcdm87@gmail.com>
2021-02-27 22:26:11 +0900

    2/27 백준(스터디) 주석 추가

--- 백준/백준 스터디/2011번 암호코드.py
+++ 백준/백준 스터디/2011번 암호코드.py
@@ -1,6 +1,6 @@
 import sys
 data = list(map(int, sys.stdin.readline().strip()))
-dp = [1] * (len(data))
+dp = [1] * (len(data)) # 해당 번째에서의 경우의 수 개수(누적될 예정)
 Continue = True
 
 
@@ -13,7 +13,7 @@ def check(x): # 경우의 수를 추가할 수 있는지 확인
 def wrong_password(x): # 올바르지 않은 암호 코드인지 확인
     if x[0] == 0: return True # 0으로 시작하면 옳지 않은 암호 코드
     for i in range(1, len(x)):
-        # 해당 문자가 0으로 끝나면서 알파벳 범위를 벗어난다면 옳지 않은 암호 코드
+        # 해당 문자가 알파벳 범위를 벗어난다면 옳지 않은 암호 코드
         if x[i] == 0 and not(1 <= x[i - 1] <= 2): return True
 
 
@@ -28,10 +28,14 @@ else:
             else:
                 # 해당 문자가 0으로 끝나지만, 알파벳 범위에 포함된다면 ex) 10, 20
                 if data[i] == 0 and check(str(data[i - 1]) + str(data[i])):
+                    # 단순히 가능한 숫자가 하나만 생성될 뿐, 여러 숫자가 만들어지지 않기 때문에, 경우의 수가 늘어나진 않는다.
+                    # 문자열의 길이가 2보다 작을 경우는 이전의 경우의 수와 동일하지만, 그 이상의 길이부터는 전전의 경우와 동일하다.
                     if i - 2 >= 0:
                         dp[i] = dp[i - 2]
                     else:
                         dp[i] = dp[i - 1]
+
+                # 해당 문자열이 알파벳 범위에 포함되지 않아, 아무런 변화가 발생하지 않음
                 else:
                     dp[i] = dp[i - 1]
 
--- 백준/백준 스터디/4195번 친구 네트워크.py
+++ 백준/백준 스터디/4195번 친구 네트워크.py
@@ -1,19 +1,22 @@
 import sys
 T = int(sys.stdin.readline().strip())
 
+# 부모노드를 찾는 함수
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
 
+# 노드를 합치는 함수
 def union_parent(parent, a, b):
+    # 각 노드의 부모 노드를 찾는다.
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    if a != b:
-        parent[b] = a
-        number[a] += number[b]
-    print(number[a])
+    if a != b: # 서로 다른 집합이라면(서로 부모 노드가 다르다면)
+        parent[b] = a # 집합을 합쳐준다. (b가 a에 속하게됨)
+        number[a] += number[b] # 집단이 합쳐졌기 때문에, b 집단의 구성원 수를 a 집단에 합쳐준다.
+    print(number[a]) # 현재 a 집단의 구성원 수 출력
 
 
 for _ in range(T):
@@ -22,6 +25,9 @@ for _ in range(T):
     number = dict()
     for _ in range(F):
         a, b = sys.stdin.readline().strip().split()
+        # 입력받은 a, b가 friend 에 존재하는지 확인
+        # 존재하지 않으면, 자기자신을 부모 노드를 설정 해주고,
+        # 본인만의 집단에 속하기 때문에, 구성원 수를 1로 설정
         if a not in friend:
             friend[a] = a
             number[a] = 1
