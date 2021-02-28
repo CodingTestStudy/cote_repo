73425fe69128e5c02453af59e1340e8fe7c3426c
choi1087 <adcdm87@gmail.com>
2021-02-28 23:38:47 +0900

    2/28 백준(스터디) 주석 추가

--- 백준/백준 스터디/10775번 공항.py
+++ 백준/백준 스터디/10775번 공항.py
@@ -1,31 +1,28 @@
 import sys
 G = int(sys.stdin.readline().strip())
 P = int(sys.stdin.readline().strip())
-plane = []
+planes = []
+parent = [i for i in range(G + 1)] # 자기자신을 부모노드로 초기화
 for _ in range(P):
-    plane.append(int(sys.stdin.readline().strip()))
+    planes.append(int(sys.stdin.readline().strip()))
 
-
-def find_parent(x):
-    if x == parent[x]:
-        return x
-    temp = find_parent(parent[x])
-    parent[x] = temp
+# 부모 노드를 찾는 함수
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
+# 두 집합을 합치는 함수
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b: parent[b] = a
+    else: parent[a] = b
 
-def union_parent(x, y):
-    x = find_parent(x)
-    y = find_parent(y)
-    parent[x] = y
-
-
-parent = {i: i for i in range(G + 1)}
 count = 0
-for i in plane:
-    x = find_parent(i)
-    if x == 0:
-        break
-    union_parent(x, x - 1)
+for plane in planes:
+    x = find_parent(parent, plane)
+    if x == 0: break # 해당 부모노드가 0이라면, 더이상 도킹할 공간이 없다는 의미
+    union_parent(parent, x, x - 1)
     count += 1
 print(count)
