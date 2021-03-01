8403476b1c93e152c72e931ceb1960c893ea8975
choi1087 <adcdm87@gmail.com>
2021-03-01 23:43:26 +0900

    3/1 백준(스터디) 주석 추가

--- 백준/백준 스터디/11053번 가장 긴 증가하는 부분수열.py
+++ 백준/백준 스터디/11053번 가장 긴 증가하는 부분수열.py
@@ -1,28 +1,12 @@
 N = int(input()) # 1 <= N <= 1,000
-# 1억 연산 1초 -> O(n^3) 알고리즘까지 아마 가능
 A = list(map(int, input().split()))
 dp = [1 for _ in range(N)] # 존재 자체만으로 길이 1
 max_value = -1
-for i in range(N):
+# 자기자신과 앞의 숫자들을 비교해가면서,
+# 본인이 더 큰 경우는 해당 번째의 길이에서 1 증가시킨 값과
+# 본인의 길이 중 더 큰 값으로 길이를 갱신해준다.
+for i in range(1, N):
     for j in range(i):
         if A[j] < A[i]:
             dp[i] = max(dp[j] + 1, dp[i])
-            
 print(max(dp))
-
-# N = int(input())
-# A = list(map(int, input().split()))
-# result = [A[0]]
-# 
-# for i in range(1, N):
-#     if result[-1] < A[i]:
-#         result.append(A[i])
-#     else:
-#         if A[i] in result:
-#             continue
-# 
-#         for j in range(len(result)):
-#             if result[j] > A[i]:
-#                 result[j] = A[i]
-#                 break
-# print(len(result))
--- 백준/백준 스터디/11055번 가장 큰 증가 부분 수열.py
+++ 백준/백준 스터디/11055번 가장 큰 증가 부분 수열.py
@@ -4,23 +4,16 @@ dp = [0 for _ in range(N)]
 
 dp[0] = A[0]
 max_value = dp[0]
+
+# 자기자신과 앞의 숫자들을 비교해가면서,
+# 본인이 더 큰 경우는 해당 번째에 축적된 dp 값과
+# 본인의 dp의 축적된 값 중 큰 값을 dp 값으로 갱신한다.
+# 위 과정을 통해 증가 부분 수열을 찾는 동시에 해당 값들을 축적하여
+# dp에 저장된 값들 중 최대값을 찾아서 출력하면 된다.
 for i in range(1, N):
     for j in range(i):
         if A[j] < A[i]:
             dp[i] = max(dp[i], dp[j])
     dp[i] += A[i]
     max_value = max(max_value, dp[i])
-
 print(max_value)
-
-# N = int(input())
-# A = list(map(int, input().split()))
-# 
-# dp = [0] * (N + 1)
-# for i in range(N):
-#     for j in range(i):
-#         if A[i] > A[j]:
-#             dp[i] = max(dp[i], dp[j])
-#     dp[i] += A[i]
-# 
-# print(max(dp))
