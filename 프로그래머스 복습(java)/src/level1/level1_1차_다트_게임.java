cc1529ddba787b0963ab2819cfacc6e7e7d914e3
choi1087 <adcdm87@gmail.com>
2021-08-12 23:01:04 +0900

    8/12 프로그래머스 level1 복습 by java

--- 프로그래머스 복습(java)/src/level1/level1_1차_다트_게임.java
+++ 프로그래머스 복습(java)/src/level1/level1_1차_다트_게임.java
@@ -1,16 +1,12 @@
 package level1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class level1_1차_다트_게임 {
 
     static class Solution {
-        int calculate(int num, String s) {
-            if (s == "S") {
+        int calculate1(int num, String s) {
+            if (s.equals("S")) {
                 return num;
-            } else if (s == "D") {
+            } else if (s.equals("D")) {
                 return num * num;
             } else {
                 return num * num * num;
@@ -18,41 +14,39 @@ public class level1_1차_다트_게임 {
         }
 
         public int solution(String dartResult) {
-            String[] split = dartResult.split("[0-9]");
-            char[] chars = dartResult.toCharArray();
-            List<Integer> numList = new ArrayList<>();
-            List<Integer> answer = new ArrayList<>();
-            numList.add(0); // 의미없는 값
-            for (char aChar : chars) {
-                int data = (int) aChar - 48;
-                if (data >= 0 && data < 10) {
-                    numList.add(data);
-                }
-            }
-            for (int i = 1; i < numList.size(); i++) {
-                System.out.println(i + " 번째");
-                if (split[i].length() == 1) {
-                    numList.set(i, calculate(numList.get(i), split[i]));
-                } else {
-                    String s1 = split[i].substring(0, 1);
-                    String s2 = split[i].substring(1, 2);
-                    numList.set(i, calculate(numList.get(i), s1));
-                    if (s2.equals("*")) {
-                        if (i == 1) {
-                            numList.set(i, numList.get(i) * 2);
-                        } else {
-                            numList.set(i - 1, numList.get(i - 1) * 2);
-                            numList.set(i, numList.get(i) * 2);
-                        }
+            int[] answer = new int[3];
+            int index = -1;
+            for (int i = 0; i < dartResult.length(); i++) {
+                char c = dartResult.charAt(i);
+                if (Character.isDigit(c)) {
+                    index++;
+                    if (dartResult.charAt(i + 1) == '0') {
+                        answer[index] = 10;
+                        i++;
+                        continue;
                     } else {
-                        numList.set(i, -numList.get(i));
+                        answer[index] = Character.getNumericValue(c);
                     }
+                } else if (Character.isAlphabetic(c)) {
+                    int num = calculate1(answer[index], String.valueOf(c));
+                    if (i + 1 != dartResult.length() && (dartResult.charAt(i + 1) == '*' || dartResult.charAt(i + 1) == '#')) {
+                        i++;
+                        char c1 = dartResult.charAt(i);
+                        if (c1 == '*') {
+                            num *= 2;
+                            if (index != 0) {
+                                answer[index - 1] *= 2;
+                            }
+                        } else {
+                            num *= -1;
+                        }
+                    }
+                    answer[index] = num;
                 }
             }
             int result = 0;
-            for (Integer num : numList) {
-                System.out.println(num);
-                result += num;
+            for (int i : answer) {
+                result += i;
             }
             return result;
         }
@@ -61,11 +55,11 @@ public class level1_1차_다트_게임 {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.solution("1S2D*3T"));
-//        System.out.println(solution.solution("1D2S#10S"));
-//        System.out.println(solution.solution("1D2S0T"));
-//        System.out.println(solution.solution("1S*2T*3S"));
-//        System.out.println(solution.solution("1D#2S*3S"));
-//        System.out.println(solution.solution("1R2D3D#"));
-//        System.out.println(solution.solution("1D2S3T*"));
+        System.out.println(solution.solution("1D2S#10S"));
+        System.out.println(solution.solution("1D2S0T"));
+        System.out.println(solution.solution("1S*2T*3S"));
+        System.out.println(solution.solution("1D#2S*3S"));
+        System.out.println(solution.solution("1R2D3D#"));
+        System.out.println(solution.solution("1D2S3T*"));
     }
 }
