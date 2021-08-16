40cc106e3d8220532531949b5c4131d9b87d80c3
choi1087 <adcdm87@gmail.com>
2021-08-16 23:52:56 +0900

    프로그래머스 level2 N개의 최소공배수 by java

---
+++ 프로그래머스 복습(java)/src/level2/level2_N개의_최소공배수.java
@@ -0,0 +1,38 @@
+package level2;
+
+import java.math.BigInteger;
+
+public class level2_N개의_최소공배수 {
+    static class Solution {
+        int gcd(int x, int y) {
+            int temp, n;
+            if (x < y) {
+                temp = x;
+                x = y;
+                y = temp;
+            }
+            while (y != 0) {
+                n = x % y;
+                x = y;
+                y = n;
+            }
+            return x;
+        }
+
+        public int solution(int[] arr) {
+            int answer = arr[0];
+            for (int i : arr) {
+                answer = answer * i / gcd(answer, i);
+            }
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr1 = {2, 6, 8, 14};
+        int[] arr2 = {1, 2, 3};
+        System.out.println(solution.solution(arr1));
+        System.out.println(solution.solution(arr2));
+    }
+}
