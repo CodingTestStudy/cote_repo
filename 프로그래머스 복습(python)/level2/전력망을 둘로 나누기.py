3054062f92f88e8ab1d2aa943fbbe19bfb0b0402
choi1087 <adcdm87@gmail.com>
2022-03-15 16:09:53 +0900

    level2 - 전력망을 둘로 나누기

---
+++ 프로그래머스 복습(python)/level2/전력망을 둘로 나누기.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+
+def find_parent(parent, x):
+    if x != parent[x]:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, x, y):
+    x = find_parent(parent, x)
+    y = find_parent(parent, y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+
+def solution(n, wires):
+    answer = 100
+    for i in range(len(wires)):
+        parent = [k for k in range(0, n + 1)]
+
+        for j in range(len(wires)):
+            if i != j:
+                x, y = wires[j]
+                if find_parent(parent, x) != find_parent(parent, y):
+                    union_parent(parent, x, y)
+
+        value_dict = defaultdict(int)
+        for index in range(1, n + 1):
+            value_dict[find_parent(parent, index)] += 1
+        value_list = list(value_dict.values())
+        first, second = value_list[0], value_list[1]
+        answer = min(answer, abs(first - second))
+    return answer
+
+
+print(solution(9, [[1, 3], [2, 3], [3, 4], [4, 5], [4, 6], [4, 7], [7, 8], [7, 9]]))
+print(solution(4, [[1, 2], [2, 3], [3, 4]]))
+print(solution(7, [[1, 2], [2, 7], [3, 7], [3, 4], [4, 5], [6, 7]]))
