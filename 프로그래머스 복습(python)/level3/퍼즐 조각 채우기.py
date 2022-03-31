0915639c5424cdcfe5d7efb667277cd5fa22e728
choi1087 <adcdm87@gmail.com>
2022-03-31 12:35:36 +0900

    level3 - 퍼즐 조각 채우기(정답 코드) - bfs, defaultdict, set() 사용

--- 프로그래머스 복습(python)/level3/퍼즐 조각 채우기.py
+++ 프로그래머스 복습(python)/level3/퍼즐 조각 채우기.py
@@ -1,89 +1,73 @@
-import copy
+from collections import defaultdict, deque
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
 
-def rotated(board):
-    length = len(board)
-    result = []
-    for b in board:
-        result.append([b[1], length - 1 - b[0]])
-    return sorted(move_to_standard(result, length))
-
-
-def dfs(table, r, c, temp, a, b):
-    if r < 0 or r >= n or c < 0 or c >= n:
-        return False
-    if table[r][c] == a:
-        temp.append([r, c])
-        table[r][c] = b
+def bfs(start, end, board, visited, target):
+    n = len(board)
+    q = deque([(start, end)])
+    ret = {(0, 0)}
+    while q:
+        r, c = q.popleft()
         for i in range(4):
-            dfs(table, r + dr[i], c + dc[i], temp, a, b)
-        return True
-    return False
+            nr, nc = r + dr[i], c + dc[i]
+            # 범위 이탈
+            if nr < 0 or nr >= n or nc < 0 or nc >= n:
+                continue
+            if board[nr][nc] == target and not visited[nr][nc]:
+                visited[nr][nc] = True
+                q.append((nr, nc))
+
+                # [0, 0] 위치를 기준
+                ret.add((nr - start, nc - end))
+    return ret
 
 
-# [0. 0] 기준으로 도형, 빈 공간 옮기기
-def move_to_standard(board, length):
-    change_list = []
-    min_r = length
-    min_c = length
+def rotate(board, n):
+    result = set()
     for r, c in board:
-        min_r = min(min_r, r)
-        min_c = min(min_c, c)
-    for r, c, in board:
-        change_list.append([r - min_r, c - min_c])
-    return sorted(change_list)
+        result.add((c, n - r - 1))
+    return result
 
 
-def solution(game_board, table):
-    global n
-    n = len(table)
+def find_block(block, set_block, n):
+    for _ in range(4):
+        mr, mc = min(block)
+        new_block = set()
+        for r, c in block:
+            new_block.add((r - mr, c - mc))
+        if new_block in set_block:
+            set_block.remove(new_block)
+            return len(new_block)
+
+        block = rotate(block, n)
+    return 0
+
+
+def solution(game, table):
     answer = 0
+    num_of_block = defaultdict(list)
+    n = len(table)
 
-    # game_board 내의 빈 공간 리스트
-    game_list = []
+    visited = [[False] * n for _ in range(n)]
+    # 블럭 개수별로 블럭의 위치 저장
     for r in range(n):
         for c in range(n):
-            if table[r][c] == 0:
-                g = []
-                dfs(game_board, r, c, g, 0, 1)
-                if g:
-                    game_list.append(g)
-
-    # [0, 0] 으로 위치 이동
-    changed_game_list = []
-    for emp in game_list:
-        changed_game_list.append(move_to_standard(emp, n))
-
-    # table 내의 조각 리스트
-    table_list = []
+            if table[r][c] == 1 and not visited[r][c]:
+                visited[r][c] = True
+                block = bfs(r, c, table, visited, 1)
+                num_of_block[len(block)].append(block)
+    # 빈 공간 찾기
+    visited = [[False] * n for _ in range(n)]
     for r in range(n):
         for c in range(n):
-            if table[r][c] == 1:
-                t = []
-                dfs(table, r, c, t, 1, 0)
-                if t:
-                    table_list.append(t)
+            if game[r][c] == 0 and not visited[r][c]:
+                visited[r][c] = True
+                empty_block = bfs(r, c, game, visited, 0)
+                if len(empty_block) in num_of_block:
+                    answer += find_block(empty_block, num_of_block[len(empty_block)], n)
 
-    changed_table_list = []
-    for sh in table_list:
-        changed_table_list.append(move_to_standard(sh, n))
-
-    for game in changed_game_list:
-        flag = False
-        for table in changed_table_list:
-            temp = copy.copy(table)
-            for _ in range(4):
-                temp = rotated(temp)
-                if game == temp:
-                    answer += len(game)
-                    changed_table_list.remove(table)
-                    flag = True
-                    break
-            if flag:
-                break
     return answer
 
 
