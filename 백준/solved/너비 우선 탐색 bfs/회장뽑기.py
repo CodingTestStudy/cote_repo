f3eacb54cccdc7611be4b9f7866fe741e0868965
choi1087 <adcdm87@gmail.com>
2022-04-26 17:32:47 +0900

    2660번 - 회장뽑기 - bfs

--- 백준/solved/너비 우선 탐색 bfs/회장뽑기.py
+++ 백준/solved/너비 우선 탐색 bfs/회장뽑기.py
@@ -3,37 +3,38 @@ from collections import deque
 
 input = sys.stdin.readline
 n = int(input())
-graph = [[1e9] * (n + 1) for _ in range(n + 1)]
-
+graph = [[] * (n + 1)  for _ in range(n + 1)]
 while True:
     x, y = map(int, input().split())
     if x == -1 and y == -1:
         break
-    graph[x][y] = 1
-    graph[y][x] = 1
+    graph[x].append(y)
+    graph[y].append(x)
 
-# 자기 자신 경로
-for i in range(1, n + 1):
-    graph[i][i] = 0
 
-# 플로이드 와샬
-for k in range(1, n + 1):
-    for r in range(1, n + 1):
-        for c in range(1, n + 1):
-            # 직접 연결되어 있거나 자기 자신인 경우 -> 제외
-            if graph[r][c] == 1 or graph[r][c] == 0:
-                continue
-            if graph[r][c] > graph[r][k] + graph[k][c]:
-                graph[r][c] = graph[r][k] + graph[k][c]
+def bfs(i):
+    # 출발지점 i
+    visited = [-1] * (n + 1)
+    visited[i] = 0
+    q = deque([i])
+    while q:
+        target = q.popleft()
+        for v in graph[target]:
+            if visited[v] == -1:
+                visited[v] = visited[target] + 1
+                q.append(v)
+    return max(visited)
 
+
+score = 1e9
 candidate = []
-for i in range(1, n + 1):
-    candidate.append(max(graph[i][1:]))
 
-min_value = min(candidate)
-answer_list = []
-for i in range(n):
-    if candidate[i] == min_value:
-        answer_list.append(i + 1)
-print(min_value, len(answer_list))
-print(*answer_list)
+for i in range(1, n + 1):
+    temp = bfs(i)
+    if temp < score:
+        score = temp
+        candidate = [i]
+    elif temp == score:
+        candidate.append(i)
+print(score, len(candidate))
+print(*candidate)
