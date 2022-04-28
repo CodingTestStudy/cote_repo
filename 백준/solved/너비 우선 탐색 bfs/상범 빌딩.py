5adfa3f6c8eff792dfae097552bfa8caf03014d3
choi1087 <adcdm87@gmail.com>
2022-04-28 10:53:09 +0900

    6593번 - 상범 빌딩(수정) - print문 오타;;;;

--- 백준/solved/너비 우선 탐색 bfs/상범 빌딩.py
+++ 백준/solved/너비 우선 탐색 bfs/상범 빌딩.py
@@ -4,32 +4,36 @@ from collections import deque
 input = sys.stdin.readline
 dr = [-1, 1, 0, 0, 0, 0]
 dc = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
+dl = [0, 0, 0, 0, -1, 1]
 
 
 def is_success(time):
-    print(f"Escaped in {time} minutes(s)")
+    print(f"Escaped in {time} minute(s).")
 
 
 def is_fail():
     print("Trapped!")
 
 
-def bfs(start_z, start_r, start_c, building, visited):
-    visited[start_z][start_r][start_c] = 1
+def bfs(ll, rr, cc):
     q = deque()
-    q.append((start_z, start_r, start_c))
+    q.append((ll, rr, cc))
+    visited[ll][rr][cc] = 1
     while q:
-        z, r, c = q.popleft()
+        l, r, c = q.popleft()
+        # if building[l][r][c] == 'E':
+        #     is_success(visited[l][r][c])
+        #     return
         for i in range(6):
-            nr, nc, nz = r + dr[i], c + dc[i], z + dz[i]
-            if 0 <= nr < R and 0 <= nc < C and 1 <= nz <= L:
-                if building[nz][nr][nc] == 'E':
-                    is_success(visited[z][r][c])
+            nl, nr, nc = l + dl[i], r + dr[i], c + dc[i]
+            # 범위 검사
+            if 1 <= nl <= L and 0 <= nr < R and 0 <= nc < C:
+                if building[nl][nr][nc] == 'E':
+                    is_success(visited[l][r][c])
                     return
-                if building[nz][nr][nc] == '.' and visited[nz][nr][nc] == 0:
-                    q.append((nz, nr, nc))
-                    visited[nz][nr][nc] = visited[z][r][c] + 1
+                if building[nl][nr][nc] == '.' and visited[nl][nr][nc] == 0:
+                    visited[nl][nr][nc] = visited[l][r][c] + 1
+                    q.append((nl, nr, nc))
     is_fail()
 
 
@@ -38,14 +42,13 @@ while True:
     if L == 0 and R == 0 and C == 0:
         break
     building = [[[] * C for _ in range(R)] for _ in range(L + 1)]
+    visited = [[[0] * C for _ in range(R)] for _ in range(L + 1)]
     for i in range(1, L + 1):
         building[i] = [list(input().strip()) for _ in range(R)]
         input()
 
-    visited = [[[0] * C for _ in range(R)] for _ in range(L + 1)]
-
-    for z in range(1, L + 1):
+    for l in range(1, L + 1):
         for r in range(R):
             for c in range(C):
-                if building[z][r][c] == 'S':
-                    bfs(z, r, c, building, visited)
+                if building[l][r][c] == 'S':
+                    bfs(l, r, c)
