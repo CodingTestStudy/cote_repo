276dfbb25b7a3fcc15f49c2376baec812efe0312
최규림 <adcdm87@gmail.com>
2022-10-06 13:17:53 +0900

    SWEA_3307 최장 증가 부분 수열

---
+++ SW_Expert_Academy/SWEA_3307.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 최장 증가 부분 수열
+public class SWEA_3307 {
+	static int T;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			int ans = 0;
+			int N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N];
+			int[] dp = new int[N];
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+				dp[i] = 1;
+			}
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < i; j++) {
+					if (arr[i] > arr[j]) {
+						dp[i] = Math.max(dp[i], dp[j] + 1);
+					}
+				}
+				ans = Math.max(ans, dp[i]);
+			}
+
+			System.out.println("#" + tc + " " + ans);
+		}
+	}
+}
